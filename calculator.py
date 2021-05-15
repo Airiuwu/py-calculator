951f6d35693e7160af73304bbb81b8686731902e
Airi <61052022+Airiuwu@users.noreply.github.com>
2021-05-15 02:10:07 -0400

    bruh cmon

--- calculator.py
+++ calculator.py
@@ -1,38 +1,38 @@
 import os, ctypes
 
 if os.name =='nt':
-	os.system('cls')
-	ctypes.windll.kernel32.SetConsoleTitleW(f"WHY DID I MAKE A CALCULATOR?")
+    os.system('cls')
+    ctypes.windll.kernel32.SetConsoleTitleW(f"WHY DID I MAKE A CALCULATOR?")
 
 signs = ['Add', 'Subtract', 'Multiply', 'Divide']
 symbol = (input("How do you want to combine your numbers?\n"))
 sign, output = "0", "0"
 
 if symbol not in signs:
-	print("That is not a valid choice.")
-	exit()
+    print("That is not a valid choice.")
+    exit()
 
 else:
-	first = int(input(f"What is the first number you would like to {symbol}?\n"))
-	second = int(input("And the second one?\n"))
+    first = int(input(f"What is the first number you would like to {symbol}?\n"))
+    second = int(input("And the second one?\n"))
 
 if symbol == "Add":
-	output = first + second
-	sign = "+"
+    output = first + second
+    sign = "+"
 
 elif symbol == "Subtract":
-	output = first - second
-	sign = "-"
+    output = first - second
+    sign = "-"
 
 elif symbol == "Multiply":
-	output = first * second
-	sign = "*"
+    output = first * second
+    sign = "*"
 
 elif symbol == "Divide":
-	output = first / second
-	sign = "/"
+    output = first / second
+    sign = "/"
 
 else:
-	print("wtf are you doing, you think that this program is that complex to understand what you just put there?\n")
+    print("wtf are you doing, you think that this program is that complex to understand what you just put there?\n")
 
 print(f"Your answer is: {first} {sign} {second} = {output}\n")
